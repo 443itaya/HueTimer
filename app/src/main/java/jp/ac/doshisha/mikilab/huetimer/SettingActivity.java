894bb2f5bde720c01307a25f693e123d526d991f
443itaya <yoshimi.itaya@gmail.com>
2018-11-25 23:35:59 +0900

    display ip address & reset color index

--- app/src/main/java/jp/ac/doshisha/mikilab/huetimer/SettingActivity.java
+++ app/src/main/java/jp/ac/doshisha/mikilab/huetimer/SettingActivity.java
@@ -1,6 +1,8 @@
 package jp.ac.doshisha.mikilab.huetimer;
 
 import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,6 +18,10 @@ public class SettingActivity extends AppCompatActivity {
     int time_index[] = new int[3];
     int color_index[] = new int[3];
     int flash_index[] = new int[3];
+
+    private TextView portText;
+
+    int id[] = new int[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,35 +83,41 @@ public class SettingActivity extends AppCompatActivity {
 
         //色指定
         RadioGroup group1 = (RadioGroup)findViewById(R.id.radioGroup);
-        group1.check(R.id.radioButton + color_index[0]);
+        id[0] = IndexToId(0, color_index[0]);
+        group1.check(id[0]);
         group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 // TODO Auto-generated method stub
-                color_index[0] = checkedId - R.id.radioButton;
+                color_index[0] = IdToIndex(0, checkedId);
                 RadioButton radio = (RadioButton)findViewById(checkedId);
+                Log.w("color1",String.valueOf(color_index[0]));
             }
         });
 
         RadioGroup group2 = (RadioGroup)findViewById(R.id.radioGroup2);
-        group2.check(R.id.radioButton7 + color_index[1]);
+        id[1] = IndexToId(1, color_index[1]);
+        group2.check(id[1]);
         group2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 // TODO Auto-generated method stub
-                color_index[1] = checkedId - R.id.radioButton7;
+                color_index[1] = IdToIndex(1, checkedId);
                 RadioButton radio = (RadioButton)findViewById(checkedId);
+                Log.w("color2",String.valueOf(color_index[1]));
             }
         });
 
         RadioGroup group3 = (RadioGroup)findViewById(R.id.radioGroup3);
-        group3.check(R.id.radioButton13 + color_index[2]);
+        id[2] = IndexToId(2, color_index[2]);
+        group3.check(id[2]);
         group3.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 // TODO Auto-generated method stub
-                color_index[2] = checkedId - R.id.radioButton13;
+                color_index[2] = IdToIndex(2, checkedId);
                 RadioButton radio = (RadioButton)findViewById(checkedId);
+                Log.w("color3",String.valueOf(color_index[2]));
             }
         });
 
@@ -160,7 +172,76 @@ public class SettingActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //wifi ipアドレス
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        WifiInfo w_info = wifiManager.getConnectionInfo();
+        int ip_addr_i = w_info.getIpAddress();
+        String ip_addr = ((ip_addr_i >> 0) & 0xFF) + "." + ((ip_addr_i >> 8) & 0xFF) + "." + ((ip_addr_i >> 16) & 0xFF) + "." + ((ip_addr_i >> 24) & 0xFF);
+        Log.i("Sample", "IP Address:"+ip_addr);
+//        String ip_addr = "172.20.11.98";
+        portText = findViewById(R.id.portNumber);
+        portText.setText(String.format("IP Address:" + ip_addr));
     }
+
+    int IndexToId(int num, int index){
+        int id = -1;
+        if(num == 0) {
+            if (index == 0) id = R.id.radioButton;
+            else if (index == 1) id = R.id.radioButton2;
+            else if (index == 2) id = R.id.radioButton3;
+            else if (index == 3) id = R.id.radioButton4;
+            else if (index == 4) id = R.id.radioButton5;
+            else if (index == 5) id = R.id.radioButton6;
+        }
+        else if(num == 1) {
+            if (index == 2) id = R.id.radioButton7;
+            else if (index == 1) id = R.id.radioButton8;
+            else if (index == 2) id = R.id.radioButton9;
+            else if (index == 3) id = R.id.radioButton10;
+            else if (index == 4) id = R.id.radioButton11;
+            else if (index == 5) id = R.id.radioButton12;
+        }
+        else if(num == 2) {
+            if (index == 3) id = R.id.radioButton13;
+            else if (index == 1) id = R.id.radioButton14;
+            else if (index == 2) id = R.id.radioButton15;
+            else if (index == 3) id = R.id.radioButton16;
+            else if (index == 4) id = R.id.radioButton17;
+            else if (index == 5) id = R.id.radioButton18;
+        }
+        return id;
+    }
+
+    int IdToIndex(int num, int id){
+        int index = -1;
+        if(num == 0) {
+            if (id == R.id.radioButton) index = 0;
+            else if (id == R.id.radioButton2) index = 1;
+            else if (id == R.id.radioButton3) index = 2;
+            else if (id == R.id.radioButton4) index = 3;
+            else if (id == R.id.radioButton5) index = 4;
+            else if (id == R.id.radioButton6) index = 5;
+        }
+        else if(num == 1) {
+            if (id == R.id.radioButton7) index = 0;
+            else if (id == R.id.radioButton8) index = 1;
+            else if (id == R.id.radioButton9) index = 2;
+            else if (id == R.id.radioButton10) index = 3;
+            else if (id == R.id.radioButton11) index = 4;
+            else if (id == R.id.radioButton12) index = 5;
+        }
+        else if(num == 2) {
+            if (id == R.id.radioButton13) index = 0;
+            else if (id == R.id.radioButton14) index = 1;
+            else if (id == R.id.radioButton15) index = 2;
+            else if (id == R.id.radioButton16) index = 3;
+            else if (id == R.id.radioButton17) index = 4;
+            else if (id == R.id.radioButton18) index = 5;
+        }
+        return index;
+    }
+
 
     //MainActivityへ値を返す
     @Override
